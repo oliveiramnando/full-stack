774530ce74de9c3c04843a19eafdd33ccb68c2ad
oliveiramnando <oliveiramnando@gmail.com>
2025-09-05 17:49:34 -0400

    actual controller functions

--- complete-notes-app/backend/src/controllers/notesController.js
+++ complete-notes-app/backend/src/controllers/notesController.js
@@ -1,16 +1,53 @@
+const Note = require('../models/Note.js');
 
 exports.getAllNotes = async (req,res) => {
-    res.status(200).send("You just fetched all notes");
+    try {
+        const result = await Note.find({});
+        res.status(200).send({ success: true, message: "You just fetched all notes", result });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 exports.createNote = async (req,res) => {
-    res.status(201).json({ message: "You just created a note" });
+    const { title, content } = req.body;
+    try {
+        const result = await Note.create({
+            title,
+            content
+        })
+        return res.status(201).json({ success: true, message: "Note successfully created", result });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 exports.updateNote = async (req,res) => {
-    res.status(200).json({ message: "You just updated a note" });
+    const { id } = req.params;
+    // const { title, content } = req.body;
+    try {
+        const existingPost = await Note.findById(id);
+        if (!existingPost) {
+            return res.status(404).json({ success: false, message: "Note not found" })
+        }
+        const result = await Note.findByIdAndUpdate(id, req.body);
+        return res.status(200).json({ message: "You just updated a note", result });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 exports.deleteNote = async (req,res) => {
-    res.status(200).json({ message: "You just deleted a note" });
+    const { id } = req.params;
+    try {
+        const existingPost = await Note.findById(id);
+        if (!existingPost) {
+            return res.status(404).json({ success: false, message: "Note not found" })
+        }
+        await Note.findByIdAndDelete(id);
+        const result = await Note.find({});
+        res.status(200).json({ success: true, message: "You just deleted a note", result });
+    } catch (error) {
+        console.log(error);
+    }
 }
