d76168b34db4e3f0c91f692f09a0f978062ee7cf
oliveiramnando <oliveiramnando@gmail.com>
2025-09-08 00:43:51 -0400

    homepage stuff + cors in the backend

--- complete-notes-app/backend/src/server.js
+++ complete-notes-app/backend/src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 
 const notesRoutes = require('./routes/notesRoutes.js');
 const { connectDB } = require('./config/db.js');
@@ -8,8 +9,12 @@ const rateLimiter = require('./middlewares/rateLimiter.js');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 app.use(rateLimiter);
+
 
 app.use('/api/notes', notesRoutes);
 
